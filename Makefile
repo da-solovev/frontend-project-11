ba015985db7fd5e85f67550491ceb147440c521e
da-solovev <solo.dmit1994@gmail.com>
2024-12-30 17:55:28 +0300

    install Webpack, bootstrap

--- Makefile
+++ Makefile
@@ -9,3 +9,12 @@ lint:
 
 lint-fix:
 	npx eslint . --fix
+
+dev:
+	npx webpack serve
+
+build_prod:
+	npm run build:prod
+
+build_dev:
+	npm run build:dev
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = (env) => {
+  return {
+    mode: env.mode ?? 'development',
+    entry: './src/index.js',
+    output: {
+      filename: '[name][contenthash].js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({ template: 'index.html' }),
+    ],
+    module: {
+      rules: [
+        { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+        },
+      ],
+    },
+  }
+};
